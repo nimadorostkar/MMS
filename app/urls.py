c740064a5950336d5d941d7c16835f649971c195
nimadorostkar <nimadorostkar97@gmail.com>
2021-09-07 11:54:51 +0430

    done

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     re_path(r'^.*\.html', views.pages, name='pages'),
     # The home page
     path('', views.index, name='index'),
+    # other
+    path('profile', views.profile, name='profile'),
+    path('search',views.search,name='search'),
     ]
--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from django import template
 from . import models
 from django.contrib.auth.models import User
 from .models import Profile
-#from .forms import ProfileForm, UserForm, TicketForm, InventoryForm
+from .forms import ProfileForm, UserForm
 from itertools import chain
 from django.contrib.auth import get_user_model
 from django.db import transaction
@@ -24,6 +24,69 @@ def index(request):
     s = "ssss"
     context = {'s':s}
     return render(request, 'index.html', context)
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+@login_required
+def profile(request):
+  profile = models.Profile.objects.filter(user=request.user)
+  if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            username = user_form.cleaned_data['username']
+            first_name = user_form.cleaned_data['first_name']
+            last_name = user_form.cleaned_data['last_name']
+            email = user_form.cleaned_data['email']
+            password1 = user_form.cleaned_data['password1']
+            password2 = user_form.cleaned_data['password2']
+            phone = profile_form.cleaned_data['phone']
+            address = profile_form.cleaned_data['address']
+            user_photo = profile_form.cleaned_data['user_photo']
+            user_form.save()
+            profile_form.save()
+            context = {'profile': profile,'user_form': user_form,'profile_form': profile_form }
+            return render(request, 'page-user.html', context)
+  else:
+      user_form = UserForm(instance=request.user)
+      profile_form = ProfileForm(instance=request.user.profile)
+
+  context = {
+  'profile': profile,
+  'user_form': user_form,
+  'profile_form': profile_form,
+  }
+  return render(request, 'page-user.html', context)
+
+
+
+
+
+#------------------------------------------------------------------------------
+@login_required
+def search(request):
+    if request.method=="POST":
+        search = request.POST['q']
+        if search:
+            material = models.Material.objects.filter(Q(name__icontains=search) | Q(description__icontains=search))
+            product = models.Product.objects.filter(Q(name__icontains=search))
+            station = models.Station.objects.filter(Q(name__icontains=search) | Q(description__icontains=search))
+            match = chain(material, product, station)
+            if match:
+                return render(request,'search.html', {'sr': match})
+            else:
+                messages.error(request,  '   چیزی یافت نشد ، لطفا مجددا جستجو کنید ' )
+        else:
+            return HttpResponseRedirect('/search')
+    return render(request, 'search.html', {})
+
+
+
+
 
 
 
