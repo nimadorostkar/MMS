ffa25598ca875c33a342cbffc094d3c646b1de66
nimadorostkar <nimadorostkar97@gmail.com>
2021-09-08 15:03:55 +0430

    start first test after debug

--- app/admin.py
+++ app/admin.py
@@ -4,8 +4,6 @@ from django.contrib.admin.models import LogEntry
 from .models import Profile, Mold, Manufacturer, Product, Category, Mold_type, Piece_id, MoldImage, Repair_request, RepairImage, Repair_operation, OperationImage, Manufacture_request, Component_request, ComponentImage
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin, ImportExportMixin
-from jalali_date import datetime2jalali, date2jalali
-from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin, TabularInlineJalaliMixin
 from mptt.admin import MPTTModelAdmin, TreeRelatedFieldListFilter, DraggableMPTTAdmin
 
 
