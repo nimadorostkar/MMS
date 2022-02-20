7b11f20c80727f9f4e35036526f18cf71b76407a
nimadorostkar <nimadorostkar97@gmail.com>
2022-02-20 16:10:00 +0330

    General Settings

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,9 @@ from unipath import Path
 import dj_database_url
 
 
+
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = Path(__file__).parent
@@ -38,7 +41,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount'
 ]
-
 
 
 
